d17d322079040843625f41cf34bf26494eeff2db
Fiddich <hiws99@naver.com>
2025-07-13 01:22:14 +0900

    docker compose로 환경변수 전달

--- api/src/main/java/org/fiddich/api/AutoTimetableApplication.java
+++ api/src/main/java/org/fiddich/api/AutoTimetableApplication.java
@@ -1,8 +1,13 @@
 package org.fiddich.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"org.fiddich"})
@@ -11,6 +16,14 @@ public class AutoTimetableApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoTimetableApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner checkEnv(Environment env) {
+		return args -> {
+			System.out.println("DB_URL: " + env.getProperty("DB_URL"));
+			System.out.println("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+		};
 	}
 
 }
